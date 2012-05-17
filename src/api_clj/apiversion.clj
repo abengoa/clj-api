01d5c056a3ad483276a42cf1d0bf12878a1625a7
Alberto Bengoa <abengoam@gmail.com>
2012-05-16 21:23:27 -0700

    Restructured the way of registering function versions

--- src/api_clj/apiversion.clj
+++ src/api_clj/apiversion.clj
@@ -12,6 +12,33 @@
 		  (swap! *_apiversion_cache_* (fn [prev#] (assoc prev# ~(str fname) fmap#)))
 		  (defn ~fname ~fargs ~@new-fbody)))
 
-(defmacro with-version [f-name version & body]
+(defmacro with-fn-version [f-name version & body]
 	`(binding [~f-name (get-function-version ~(str f-name) ~version)]
 		~@body))
+
+(def *_apidefinition_cache_* (atom {}))
+(defn get-api [name] (get @*_apidefinition_cache_* name {}))
+(defn set-api [name api] (swap! *_apidefinition_cache_* (fn [previous] (assoc previous name api))))
+(defn get-api-versions [api] (keys api))
+(defn get-api-version [api version] (get api version []))
+(defn add-api-version [api version fn-list] (assoc api version fn-list))
+(defn clone-api-version [api base-version new-version] (assoc api new-version (get-api-version base-version)))
+(defn delete-api-version [api version] (dissoc api version))
+
+(defmacro new-api [name fn-list] `(set-api ~name {:base (map (fn [f] `(str ~f)) fn-list)}))
+
+(defmacro with-api-version
+	[api-name version & body]
+	`(let [fns# (get-api-version (get-api ~api-name) ~version)]
+		(println `(interleave fns# (map #(get-function-version % ~version) fns#)))
+		;(interleave fns# 
+		;	(for [n fns#] (get-function-version n ~version))))
+		;(bindings [~@(interleave fns# (for [n fns#] (get-function-version n ~version)))]
+		;~@body)
+		))
+		
+(defmacro redefine-api [api-name version & body]
+	;; replace all the (defn f [x] b) for (new-version f version [x] b) 
+	;; only when f in api? or all function definitions in the body?
+	;; the new function definitions do not overwrite the old
+	)
--- test/api_clj/test/apiversion.clj
+++ test/api_clj/test/apiversion.clj
@@ -7,5 +7,13 @@
 	(is 4 (myfn 3))
 	(new-version myfn [2 0] [x] (dec x))
 	(is 2 (myfn 3))
-	(is 4 (with-version myfn :base (myfn 3)))
-	(is 2 (with-version myfn [2 0] (myfn 3))))
+	(is 4 (with-fn-version myfn :base (myfn 3)))
+	(is 2 (with-fn-version myfn [2 0] (myfn 3))))
+	
+	
+(deftest api-versioning
+	(defn f1 [x] (inc x))
+	(new-api "myapi" [f1])
+	(with-api-version "myapi" :base (println "done."))
+	)
+	
